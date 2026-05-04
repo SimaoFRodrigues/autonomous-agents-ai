4bba0825b9290936634542b9eba682c9e46a7d47
ayosimao <simao3175@gmail.com>
2026-05-04 18:12:52 +0100

    + comentários alusivos aos agentes deliberativos

--- src/agente_prosp/controlo_delib/controlo_delib.py
+++ src/agente_prosp/controlo_delib/controlo_delib.py
@@ -19,19 +19,25 @@ class ControloDelib(Controlo):
 
     def processar(self, percepcao):
         """
-        O método processar gera uma ação a realizar
+        O método processar observa o modelo do mundo e gera percepções a realizar, sendo assim
+        o primeiro passo no processo de tomada de decisão e ação.
         """
 
     def _assimilar(self, percepcao):
         """
         O método privado assimilar vai atualizar o modelo do mundo baseando-se no
-        parâmetro de percepção.
+        parâmetro de percepção, sendo o segundo passo do processo de tomada de decisão e ação.
         """
         return self.__modelo.mundo.atualizar(percepcao)
 
     def _reconsiderar(self):
         """
-        O método retorna um booleano que indica se é ou não para reconsiderar.
+        O método retorna um booleano que indica se é ou não para reconsiderar, sendo este o terceiro
+        passo no processo de tomada de decisão e ação.
+        Reconsiderar é a resolução ao problema do dinamismo do ambiente, visto que
+        o resultado pode já não ser consistente com o ambiente, gerando assim novas 
+        opções. Posteriormente se for preciso reconsiderar verifica se é necessário adaptar os 
+        objetivos e replaneia, senão mantém o plano de tomada de decisão e ação.
         """
         return self.__modelo_mundo.alterado or not self.__plano 
 
@@ -39,20 +45,21 @@ class ControloDelib(Controlo):
     def _deliberar(self):
         """
         Este método inicia os objetivos preenchendo a lista com os objetivos 
-        deliberados.
+        deliberados, sendo este o quarto passo no processo de tomada de decisão e ação.
         """
         self.__objetivos = self.__mec_delib.deliberar()
 
     def _planear(self):
         """
-        Constrói um plano de ação a partir dos objetivos
-        deliberados, delegando a tarefa ao `planeador` fornecido.
+        Constrói um plano de ação a partir dos objetivos deliberados, delegando a tarefa ao 
+        planeador fornecido e sendo o quinto passo no plano de tomada de decisão e ação.
         """
 
     def _executar(self):
         """
         Executa o plano atual, invocando as ações sobre o modelo do mundo e atualizando 
-        o plano conforme cada ação é consumida, reconsiderando se necessário.
+        o plano conforme cada ação é consumida, reconsiderando se necessário. Este é o passo final
+        do ciclo do plano de tomada de decisão e ação.
         """
 
     def _mostrar(self):
